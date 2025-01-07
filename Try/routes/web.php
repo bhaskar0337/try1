19057e94b67a76180aa74158d97b0ada454368a7
KalavalapalliMohan <kalavalapallimohan9@gmail.com>
2025-01-07 11:04:23 +0530

    new changes

--- Try/routes/web.php
+++ Try/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/index', [Controller::class, 'index'])->name('index');
